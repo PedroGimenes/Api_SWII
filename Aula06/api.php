0ad684ac0d2e10c2989a825bafb74c401287068f
PedroGimenes <phgimenes.andrade@gmail.com>
2026-04-07 15:37:42 -0300

    salvando ne tropa

---
+++ Aula06/api.php
@@ -0,0 +1,13 @@
+<?php
+    //CABEÇALHO
+    header("Content-Type: application/json"); // Define o tipo de resposta
+
+    //CONTEÚDO
+    $usuarios = [
+        ["id" => 1, "nome" => "Maria Souza", "email" => "maria@email.com"],
+        ["id" => 2, "nome" => "João Silva", "email" => "joao@email.com"]
+    ];
+
+    // Converte para JSON e retorna
+    echo json_encode($usuarios);
+?>
